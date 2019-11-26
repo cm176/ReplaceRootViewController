f583e75bd8253fda056c315fc7d6d6fe4b86185d
Cliff Mayes <cliff.mayes@voltbank.com.au>
2019-11-26 13:11:39 +1100

    Attempted fix for old controllers still existing

--- ReplaceRootViewController/Classes/ReplaceRootViewController.swift
+++ ReplaceRootViewController/Classes/ReplaceRootViewController.swift
@@ -129,7 +129,7 @@ public extension UIWindow {
 
         layer.add(options.animation, forKey: kCATransition)
 
-        rootViewController = newRootViewController
+//        rootViewController = newRootViewController
 
         // Update status bar appearance using the new view controllers appearance - animate if needed
         if UIView.areAnimationsEnabled {
@@ -140,21 +140,30 @@ public extension UIWindow {
             newRootViewController.setNeedsStatusBarAppearanceUpdate()
         }
 
- 
-        // The presenting view controllers view doesn't get removed from the window as its currently transistioning and presenting a view controller
-        if let transitionViewClass = NSClassFromString("UITransitionView") {
-            for subview in subviews where subview.isKind(of: transitionViewClass) {
-                subview.removeFromSuperview()
-            }
-        }
+//        if #available(iOS 13.0, *) {
+//            // In iOS 13 we don't want to remove the transition view as it'll create a blank screen
+//        } else {
+//            // The presenting view controllers view doesn't get removed from the window as its currently transistioning and presenting a view controller
+//            if let transitionViewClass = NSClassFromString("UITransitionView") {
+//                for subview in subviews where subview.isKind(of: transitionViewClass) {
+//                    subview.removeFromSuperview()
+//                }
+//            }
+//        }
 
-        if let previousViewController = previousViewController {
-            // Allow the view controller to be deallocated
-            previousViewController.dismiss(animated: false) {
-                // Remove the root view in case its still showing
-                previousViewController.view.removeFromSuperview()
+        let presentedViewController = rootViewController?.presentedViewController
+        presentedViewController?.present(newRootViewController, animated: true) {
+            rootViewController?.dismiss(animated: false) {
+                rootViewController = newRootViewController
             }
         }
+//        if let previousViewController = previousViewController {
+//            // Allow the view controller to be deallocated
+//            previousViewController.dismiss(animated: false) {
+//                // Remove the root view in case its still showing
+//                previousViewController.view.removeFromSuperview()
+//            }
+//        }
     }
 }
 
